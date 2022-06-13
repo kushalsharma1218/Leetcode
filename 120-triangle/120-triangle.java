df49dc97688bccd6d8c4f2e4a5b5025d91fcdaa6
Kushal Sharma <kushalsharma1218@gmail.com>
2022-06-13 12:14:28 +0530

    Time: 12 ms (11.50%), Space: 44 MB (47.12%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
@@ -20,6 +21,44 @@ class Solution {
         int right = findMinPathSum(row+1, col+1, triangle, dp);
         
         return dp[row][col] = Math.min(left, right)+triangle.get(row).get(col);
+        
+    }
+}
+*/
+
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        if(n == 1)return triangle.get(0).get(0);
+        int dp[][] = new int[n][];
+        
+        for(int i = 0;i<n;i++){
+            dp[i] = new int[triangle.get(i).size()];
+        }
+        
+        dp[0][0] = triangle.get(0).get(0);
+        int minPathSum = Integer.MAX_VALUE;
+        for(int row = 1;row<n;row++){
+            for(int col = 0;col<triangle.get(row).size();col++)
+            {
+                dp[row][col] = triangle.get(row).get(col);
+                if(col == 0){
+                    dp[row][col] = dp[row][col] + dp[row-1][col];
+                }
+                else if(col == triangle.get(row).size() -1){
+                    dp[row][col] = dp[row][col] + dp[row-1][col-1];
+                }
+                else{
+                    dp[row][col] = Math.min(dp[row][col] + dp[row-1][col], dp[row][col] + dp[row-1][col-1]);
+                }
+                
+                if(row == n-1){
+                    minPathSum = Math.min(minPathSum, dp[row][col]);
+                }
+            }
+        }
+        return minPathSum;
         
     }
 }
