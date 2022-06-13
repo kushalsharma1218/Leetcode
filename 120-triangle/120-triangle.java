5eee3c022f268d109c2a9c7fab2d9c41e18f3f32
Kushal Sharma <kushalsharma1218@gmail.com>
2022-06-13 12:24:30 +0530

    Time: 8 ms (24.53%), Space: 45 MB (8.91%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -26,39 +26,23 @@ class Solution {
 }
 */
 
-
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        if(n == 1)return triangle.get(0).get(0);
-        int dp[][] = new int[n][];
-        
-        for(int i = 0;i<n;i++){
-            dp[i] = new int[triangle.get(i).size()];
-        }
-        
-        dp[0][0] = triangle.get(0).get(0);
-        int minPathSum = Integer.MAX_VALUE;
-        for(int row = 1;row<n;row++){
-            for(int col = 0;col<triangle.get(row).size();col++)
-            {
-                dp[row][col] = triangle.get(row).get(col);
-                if(col == 0){
-                    dp[row][col] = dp[row][col] + dp[row-1][col];
+        List<Integer> prevRow = triangle.get(0);
+        for (int row = 1; row < triangle.size(); row++) {
+            List<Integer> currRow = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                int smallestAbove = Integer.MAX_VALUE;           
+                if (col > 0) {
+                    smallestAbove = prevRow.get(col - 1);
+                } 
+                if (col < row) {
+                    smallestAbove = Math.min(smallestAbove, prevRow.get(col));
                 }
-                else if(col == triangle.get(row).size() -1){
-                    dp[row][col] = dp[row][col] + dp[row-1][col-1];
-                }
-                else{
-                    dp[row][col] = Math.min(dp[row][col] + dp[row-1][col], dp[row][col] + dp[row-1][col-1]);
-                }
-                
-                if(row == n-1){
-                    minPathSum = Math.min(minPathSum, dp[row][col]);
-                }
+                currRow.add(smallestAbove + triangle.get(row).get(col));
             }
+            prevRow = currRow;
         }
-        return minPathSum;
-        
+        return Collections.min(prevRow);
     }
 }
