72c738d000786f86298543e8b048ca2665154c4a
Kushal Sharma <kushalsharma1218@gmail.com>
2022-06-14 12:39:12 +0530

    Time: 25 ms (9.27%), Space: 47 MB (40.58%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -14,8 +14,6 @@ class Solution {
     }
 }
 
-*/
-
 class Solution {
     public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length()][word2.length()];
@@ -23,6 +21,43 @@ class Solution {
             Arrays.fill(dp[i], -1);
         }
         return minDeletions(word1, word2, 0, 0, dp);
+    }
+    public int minDeletions(String word1, String word2, int ind1, int ind2, int dp[][]){
+        if(ind1 >= word1.length() && ind2 >= word2.length())return 0;
+        if(ind1 >= word1.length())return word2.length() - ind2;
+        if(ind2 >= word2.length())return word1.length() - ind1;
+        
+        if(dp[ind1][ind2] != -1)return dp[ind1][ind2];
+        
+        if(word1.charAt(ind1) == word2.charAt(ind2))return dp[ind1][ind2] = minDeletions(word1,word2,ind1+1,ind2+1, dp);
+        
+        return dp[ind1][ind2] = Math.min(minDeletions(word1,word2,ind1+1,ind2, dp), minDeletions(word1,word2,ind1,ind2+1,dp)) + 1;
+    }
+}
+
+*/
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        
+        
+        for(int i = 0;i<=word1.length();i++){
+            for(int j = 0;j<=word2.length();j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + 1;
+                }
+            }
+        }
+        
+        return dp[word1.length()][word2.length()];
+        
     }
     public int minDeletions(String word1, String word2, int ind1, int ind2, int dp[][]){
         if(ind1 >= word1.length() && ind2 >= word2.length())return 0;
