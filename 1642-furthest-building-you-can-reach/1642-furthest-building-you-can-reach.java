4889c6aeedc297b84c77951b656dd04e1329b651
Kushal Sharma <kushalsharma1218@gmail.com>
2022-06-21 19:11:32 +0530

    Time: 23 ms (75.06%), Space: 51.6 MB (90.42%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        // Create a priority queue with a comparator that makes it behave as a min-heap.
+        Queue<Integer> ladderAllocations = new PriorityQueue<>((a, b) -> a - b);
+        for (int i = 0; i < heights.length - 1; i++) {
+            int climb = heights[i + 1] - heights[i];
+            // If this is actually a "jump down", skip it.
+            if (climb <= 0) {
+                continue;
+            }
+            // Otherwise, allocate a ladder for this climb.
+            ladderAllocations.add(climb);
+            // If we haven't gone over the number of ladders, nothing else to do.
+            if (ladderAllocations.size() <= ladders) {
+                continue;
+            }
+            // Otherwise, we will need to take a climb out of ladder_allocations
+            bricks -= ladderAllocations.remove();
+            // If this caused bricks to go negative, we can't get to i + 1
+            if (bricks < 0) {
+                return i;
+            }
+        }
+        // If we got to here, this means we had enough materials to cover every climb.
+        return heights.length - 1;
+    }
+}
